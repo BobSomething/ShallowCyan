5c638b873d0114be8a22f8acfe00883e4b4a4275
Bob <bob@Bob.A>
2024-12-01 17:19:33 +0100

    implemented pretty much every legal move, except: en passant, pawn promotion and king checks

--- piece.cpp
+++ piece.cpp
@@ -1,23 +1,36 @@
 #include "piece.hpp"
 
-array_coords piece_t::legal_moves(board_t* board) {
+
+void error(std::string str) {
+    std::cout << "Error: " << str << std::endl;
+    exit(1);
+}
+
+
+array_coords piece_t::legal_moves() {
     array_coords arr;
     return arr;
 }
 
-bool piece_t::check_cell(int x, int y, board_t* board) {
-    if(x >= 7 || x < 0 || y >= 7 || y < 0)
-        return false;
+int piece_t::check_cell(int x, int y) {
+    if(x >= SIZE || x < 0 || y >= SIZE || y < 0) //the cell is out of bounds
+        return 0;
 
-    if(board->state[x][y]->color == color)
-        return false;
+    if(board->state[x][y] == nullptr) //the cell is empty
+        return 1;
 
-    return true; 
+    if(board->state[x][y]->color == color) //the cell has a piece with the same color
+        return 0;
+
+    if(board->state[x][y]->color == !color) //the cell has a piece with the opposite color
+        return 2;
+    
+    error("Somehow the board is not implement correctly");
 }
 
 //implementation of legal moves
 //TODO NEED TO IMPLEMENT EN PASSANT
-array_coords pawn_t::legal_moves(board_t* board){
+array_coords pawn_t::legal_moves(){
     array_coords arr;
     int d = 1; //direction
     int first_time = 1; //the pawn did not move yet
@@ -36,22 +49,20 @@ array_coords pawn_t::legal_moves(board_t* board){
         //PROMOTION!
     }
 
-    if (board->state[x+d][y] == nullptr){ //if next square is empty
-        arr.push_back(std::make_pair(x+d,y));
+    if(check_cell(x+d,y) == 0){ //if next square is empty
+        arr.push_back(makep(x+d,y));
 
         if (x == first_time) //first move: you can move two if square is empty
-            if(board->state[x+2*d][y] == nullptr)
-                arr.push_back(std::make_pair(x+2*d,y));
+            if(check_cell(x+2*d,y) == 0)
+                arr.push_back(makep(x+2*d,y));
     }
     
     //checking if the diagonal captures are legal
-    if(check_cell(x+d,y+1,board)) 
-        if(board->state[x+d][y+1]->color == !color)
-            arr.push_back(std::make_pair(x+d,y+1));
+    if(check_cell(x+d,y+1) == 2) 
+        arr.push_back(makep(x+d,y+1));
 
-    if(check_cell(x+d,y-1,board)) 
-        if(board->state[x+d][y-1]->color == !color)
-            arr.push_back(std::make_pair(x+d,y-1));
+    if(check_cell(x+d,y-1) == 2) 
+        arr.push_back(makep(x+d,y-1));
 
     /*
     if (x < 7){ //not go outside of the board
@@ -102,36 +113,167 @@ array_coords pawn_t::legal_moves(board_t* board){
     return arr;
 }
 
-array_coords bishop_t::legal_moves(board_t* board) {
-    //TODO
+array_coords bishop_t::legal_moves() {
     array_coords arr;
+    int x = location.i;
+    int y = location.j;
+    int it = 0;
+    std::map<coords, bool> directions;
+    directions[makep(1,1)] = true;
+    directions[makep(-1,1)] = true;
+    directions[makep(1,-1)] = true;
+    directions[makep(-1,-1)] = true;
+
+    while(true) {
+        bool is_going = false;
+        it++;
+        //we iterate for every possible directions
+        for(auto &[key, value]: directions) {
+            if(value == true) {
+                is_going = true;
+                //if it is out of bounds or lands on a cell with the same color piece
+                if(check_cell(x+it*key.i,y+it*key.j) == 0) {
+                    directions[key] = false;
+                }
+
+                //if it lands on a cell with a opposite color piece
+                else if(check_cell(x+it*key.i,y+it*key.j) == 2) {
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+                    directions[key] = false;
+                }
+                //if it lands on a empty cell
+                else
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+            }
+        }
+        if(is_going == false)
+            break;
+    }
     return arr;
 }
 
 
-array_coords knight_t::legal_moves(board_t* board) {
-    //TODO
+array_coords knight_t::legal_moves() {
     array_coords arr;
+    int x = location.i;
+    int y = location.j;
+    coords directions[] = {makep(2,1), makep(2,-1), makep(-2,1), makep(-2,-1), makep(1,2), makep(1,-2), makep(-1,2), makep(-1,-2)};
+    for(auto& [i, j]: directions) {
+        if(check_cell(x+i,y+j) != 0) {
+            arr.push_back(makep(x+i,y+j));
+        }
+    }
     return arr;
 }
 
 
-array_coords rook_t::legal_moves(board_t* board) {
-    //TODO
+array_coords rook_t::legal_moves() {
     array_coords arr;
+    int x = location.i;
+    int y = location.j;
+    int it = 0;
+    std::map<coords, bool> directions;
+    directions[makep(1,0)] = true;
+    directions[makep(-1,0)] = true;
+    directions[makep(0,-1)] = true;
+    directions[makep(0,-1)] = true;
+
+    while(true) {
+        bool is_going = false;
+        it++;
+        //we iterate for every possible directions
+        for(auto &[key, value]: directions) {
+            if(value == true) {
+                is_going = true;
+                //if it is out of bounds or lands on a cell with the same color piece
+                if(check_cell(x+it*key.i,y+it*key.j) == 0) {
+                    directions[key] = false;
+                }
+
+                //if it lands on a cell with a opposite color piece
+                else if(check_cell(x+it*key.i,y+it*key.j) == 2) {
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+                    directions[key] = false;
+                }
+                //if it lands on a empty cell
+                else
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+            }
+        }
+        if(is_going == false)
+            break;
+    }
     return arr;
 }
 
 
-array_coords king_t::legal_moves(board_t* board) {
-    //TODO
+array_coords king_t::legal_moves() {
+    //Can simplify this
+    //NEED TO FIX THIS, CHECK IF THE KING IS NOT GOING ON A CHECK
     array_coords arr;
+    int x = location.i;
+    int y = location.j;
+    int it = 0;
+    std::map<coords, bool> directions;
+    directions[makep(1,1)] = true;
+    directions[makep(-1,1)] = true;
+    directions[makep(1,-1)] = true;
+    directions[makep(-1,-1)] = true;
+    
+    directions[makep(1,0)] = true;
+    directions[makep(-1,0)] = true;
+    directions[makep(0,-1)] = true;
+    directions[makep(0,-1)] = true;
+
+    for(auto &[key, value]: directions) {
+        //if it lands on a cell with a opposite color piece or if it lands on a empty cell
+        if(check_cell(x+it*key.i,y+it*key.j) == 2 || check_cell(x+it*key.i,y+it*key.j) == 1)
+            arr.push_back(makep(x+it*key.i,y+it*key.j));
+    }
     return arr;
 }
 
 
-array_coords queen_t::legal_moves(board_t* board) {
-    //TODO
+array_coords queen_t::legal_moves() {
     array_coords arr;
+    int x = location.i;
+    int y = location.j;
+    int it = 0;
+    std::map<coords, bool> directions;
+    directions[makep(1,1)] = true;
+    directions[makep(-1,1)] = true;
+    directions[makep(1,-1)] = true;
+    directions[makep(-1,-1)] = true;
+
+    directions[makep(1,0)] = true;
+    directions[makep(-1,0)] = true;
+    directions[makep(0,-1)] = true;
+    directions[makep(0,-1)] = true;
+
+    while(true) {
+        bool is_going = false;
+        it++;
+        //we iterate for every possible directions
+        for(auto &[key, value]: directions) {
+            if(value == true) {
+                is_going = true;
+                //if it is out of bounds or lands on a cell with the same color piece
+                if(check_cell(x+it*key.i,y+it*key.j) == 0) {
+                    directions[key] = false;
+                }
+
+                //if it lands on a cell with a opposite color piece
+                else if(check_cell(x+it*key.i,y+it*key.j) == 2) {
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+                    directions[key] = false;
+                }
+                //if it lands on a empty cell
+                else
+                    arr.push_back(makep(x+it*key.i,y+it*key.j));
+            }
+        }
+        if(is_going == false)
+            break;
+    }
     return arr;
 }
--- piece.hpp
+++ piece.hpp
@@ -3,69 +3,76 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include "board.hpp"
 #include "macro.hpp"
 
 struct board_t;
 
+void error(std::string str) __attribute__((noreturn));
 
 struct piece_t {
     bool color; //1 - white and 0 - black
     int points; //counts how valueable is a piece
     coords location; //location on the board, if there will be no other methods, just put it in the legal_moves
+    board_t* board; //the pointer to the board
 
     /* vector of pairs (i,j), indicating the cells */
-    virtual array_coords legal_moves(board_t* board); 
+    virtual array_coords legal_moves(); 
 
     /* constructor of piece_t */
-    piece_t(coords location, bool color)
-        : location {location}, color {color}, points {0} {}
+    piece_t(coords location, bool color, board_t* board)
+        : location {location}, color {color}, points {0}, board {board} {}
 
-    /* returns false if the cell is out of bounds and if the cell contains a piece with the same color */
-    bool check_cell(int x, int y, board_t* board);
+    /* 
+    returns 0 if the cell is out of bounds or if the cell in bounds and contains a piece with the same color 
+    returns 1 if the cell is in bounds and it is empty
+    returns 2 if the cell is in bounds and contains a piece with opposite color
+    */
+    int check_cell(int x, int y);
 };
 
 
 struct pawn_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    pawn_t(coords location, bool color)
-        : piece_t(location, color) {points = 100;}
+    pawn_t(coords location, bool color, board_t* board)
+        : piece_t(location, color, board) {points = 100;}
 };
 
 struct bishop_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    bishop_t(coords location, bool color)
-        : piece_t(location, color) {points = 300;}
+    bishop_t(coords location, bool color , board_t* board)
+        : piece_t(location, color, board) {points = 300;}
 };
 
 struct knight_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    knight_t(coords location, bool color)
-        : piece_t(location, color) {points = 300;}
+    knight_t(coords location, bool color, board_t* board)
+        : piece_t(location, color, board) {points = 300;}
 };
 
 struct rook_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    rook_t(coords location, bool color)
-        : piece_t(location, color) {points = 500;}
+    rook_t(coords location, bool color, board_t* board)
+        : piece_t(location, color, board) {points = 500;}
 };
 
 struct king_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    king_t(coords location, bool color)
-        : piece_t(location, color) {points = 0;}
+    king_t(coords location, bool color, board_t* board)
+        : piece_t(location, color, board) {points = 0;}
 };
 
 struct queen_t : piece_t {
-    array_coords legal_moves(board_t* board);
+    array_coords legal_moves();
 
-    queen_t(coords location, bool color)
-        : piece_t(location, color) {points = 900;}
+    queen_t(coords location, bool color, board_t* board)
+        : piece_t(location, color, board) {points = 900;}
 };
 
 #endif
