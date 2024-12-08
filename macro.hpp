096e460e2ba57ba1139c0e15205d4f537fbecef9
Bob <bob@Bob.A>
2024-12-08 03:36:28 +0100

    change array_coords to array_moves

--- macro.hpp
+++ macro.hpp
@@ -1,7 +1,14 @@
+#ifndef MACRO_HPP_
+#define MACRO_HPP_
+
+struct move_t;
+
 #define coords std::pair<int,int> //location of a piece
-#define array_coords std::vector<coords>  //vector of coordinates
+#define array_moves std::vector<move_t>  //vector of move_t
 #define i first //number
 #define j second //letter
 #define makep std::make_pair
 #define SIZE 8
 #define inf 2147483647
+
+#endif
