7090c9203bbd04397d1e01a9e2fffe583b98eceb
Bob <bob@Bob.A>
2024-12-09 22:56:26 +0100

    changing move_t to move_t*

--- macro.hpp
+++ macro.hpp
@@ -4,7 +4,7 @@
 struct move_t;
 
 #define coords std::pair<int,int> //location of a piece
-#define array_moves std::vector<move_t>  //vector of move_t
+#define array_moves std::vector<move_t*>  //vector of move_t
 #define i first //number
 #define j second //letter
 #define makep std::make_pair
