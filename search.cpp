fba1756b4d492873ffce2d7258d242b4b544fa37
yonas.grossard-amin <yonas.grossard-amin@polytechnique.edu>
2025-01-06 14:08:33 +0100

    seach.cpp

--- search.cpp
+++ search.cpp
@@ -21,9 +21,9 @@ eval_move bitboard_t::search_aux(int depth, int α, int β) {
 
     if (turn) {
 		int val = -inf;
-		eval_move end = eval_move(val, nullptr);
 		std::vector<move_t*> moves;
 		generate_all_moves(&moves);
+		eval_move end = eval_move(val, moves[0]);
         for (move_t* move: moves) {
 			int p_before = pieceTable[move->before.i*8 + move->before.j];
 			int p_after = pieceTable[move->after.i*8 + move->after.j]; 
@@ -54,9 +54,9 @@ eval_move bitboard_t::search_aux(int depth, int α, int β) {
 	}
     else {
 		int val = inf;
-		eval_move end = eval_move(val, nullptr);
 		std::vector<move_t*> moves;
 		generate_all_moves(&moves);
+		eval_move end = eval_move(val, moves[0]);
         for (move_t* move : moves) {
 			int p_before = pieceTable[move->before.i*8 + move->before.j];
 			int p_after = pieceTable[move->after.i*8 + move->after.j]; 
