88e85019b82a7e252159da7be378278e2a9419bc
Sacha <sacha.gregoire@polytechnique.edu>
2025-01-16 11:01:32 +0100

    seeaaaaaaaaaaarch but better

--- search.cpp
+++ search.cpp
@@ -1,89 +1,61 @@
 #include "bitboard.hpp"
 
-move_t* bitboard_t::search(int depth, int α, int β) {
+move_t* bitboard_t::search(int depth, int α = -inf, int β = inf) {
 	/* Alpha-beta pruning :) */
     if (depth == 0) { 
-		return eval_move(this->eval(),nullptr);
+		move_t* ret;
+		ret->eval = this->eval();
+		return ret;
 	}
 
 	if(no_moves()) {
 		int king = (turn == 0) ? 11 : 5;
 		int sign = (turn == 0) ? 1 : -1;
-		//checkmate
-		if(is_square_attacked(get_LSB(piecesBB[king]),!turn)) {
-			return eval_move(sign*inf,nullptr);
-		}
-		//stalemate
-		return eval_move(0,nullptr);
+		move_t* ret;
+		if(is_square_attacked(get_LSB(piecesBB[king]),!turn)) ret->eval = sign*inf; //checkmate
+		else ret->eval = 0; //stalemate
+		return ret;
 	}
 
-    if (turn) {
-		int val = -inf;
-		std::vector<move_t*> moves;
-		generate_all_moves(&moves);
-		eval_move end = eval_move(val, moves[0]);
-        for (move_t* move: moves) {
-			int p_before = pieceTable[move->before.i*8 + move->before.j];
-			int p_after = pieceTable[move->after.i*8 + move->after.j]; 
-			int ep_square = enpassant_square;
-			bool w_c_kside = w_castle_kside;
-			bool w_c_qside = w_castle_qside; 
-			bool b_c_kside = b_castle_kside;
-			bool b_c_qside = b_castle_qside;
+    int val = (turn) ? -inf : inf;
+	std::vector<move_t*> moves;
+	generate_all_moves(&moves);
+	move_t* ret;
+	ret->eval = val;
+	for (move_t* move: moves) {
+		int p_before = pieceTable[move->before.i*8 + move->before.j];
+		int p_after = pieceTable[move->after.i*8 + move->after.j]; 
+		int ep_square = enpassant_square;
+		bool w_c_kside = w_castle_kside;
+		bool w_c_qside = w_castle_qside; 
+		bool b_c_kside = b_castle_kside;
+		bool b_c_qside = b_castle_qside;
 
-			//bitboard_t child = this->copy();
-			//child.update(move);
-			update(move);
+		//bitboard_t child = this->copy();
+		//child.update(move);
+		update(move);
 
-			//int pval = child.search_aux(depth - 1, α, β).first;
-			int pval = search_aux(depth - 1, α, β).first;
+		//int pval = child.search_aux(depth - 1, α, β).first;
+		int pval = search_aux(depth - 1, α, β)->eval;
 
-			undo(move,p_before,p_after,ep_square,w_c_kside,w_c_qside,b_c_kside,b_c_qside);
-
+		undo(move,p_before,p_after,ep_square,w_c_kside,w_c_qside,b_c_kside,b_c_qside);
+		
+		if (turn) {
 			if (pval > val){
 				val = pval;
-				end = eval_move(val, move);
+				move->eval = val;
+				ret = move;
 			} 
 			if (val > β) break;
 			if (val > α) α = val;
-			
-		}
-        return end;
-	}
-    else {
-		int val = inf;
-		std::vector<move_t*> moves;
-		generate_all_moves(&moves);
-		eval_move end = eval_move(val, moves[0]);
-        for (move_t* move : moves) {
-			int p_before = pieceTable[move->before.i*8 + move->before.j];
-			int p_after = pieceTable[move->after.i*8 + move->after.j]; 
-			int ep_square = enpassant_square;
-			bool w_c_kside = w_castle_kside;
-			bool w_c_qside = w_castle_qside; 
-			bool b_c_kside = b_castle_kside;
-			bool b_c_qside = b_castle_qside;
-
-			//bitboard_t child = this->copy();
-			//child.update(move);
-			update(move);
-
-			//int pval = child.search_aux(depth - 1, α, β).first;
-			int pval = search_aux(depth - 1, α, β).first;
-
-			undo(move,p_before,p_after,ep_square,w_c_kside,w_c_qside,b_c_kside,b_c_qside);
-
+		} else {
 			if (pval < val){
 				val = pval;
-				end = eval_move(val, move);
+				move->eval = val;
+				ret = move;
 			} 
 			if (val < α) break;
 			if (val < β) β = val;
 		}
-        return end;
-	} 
-}
-
-move_t* bitboard_t::search(int depth){
-	return search_aux(depth).second;
+    return ret;
 }
